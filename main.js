d987b9b75564308f916dc7ffa229207eff404bb0
jagruti1997-jn <99661287+jagruti1997-jn@users.noreply.github.com>
2022-02-17 22:09:08 +0530

    ok

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $nav = $('.nav');
+    $toggleCollapse=$('.toggle-collapse');
+
+
+
+/**click event on toggle menu */
+$toggleCollapse.click(function(){
+    $nav.toggleClass('collapse');
+})
+
+//owl-carousel 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout: 3000 ,
+    nav:true,
+    navText:[$('owl-navigation .owl-nav-prev'),$('owl-navigation .owl-nav-next')]
+});
+
+});
